b3ee36cff6fff101ecde35a4e9c928ae9b85b991
matisla <herrmann.matthieu@gmail.com>
2014-03-24 15:55:17 +0100

    update

--- Hexapod_Semesterarbeit/GUI.py
+++ Hexapod_Semesterarbeit/GUI.py
@@ -14,64 +14,138 @@ Option:
 from tkinter import *
 import cmdServos
 
+class User_Interface(Frame):
+    
+    def __init__(self):
+        
+        self.init_loger()
+        self.init_controler()
+        self.init_sender()
+        
+        self.fenetre.mainloop()
 
-fenetre = Tk()
-fenetre.title("Commande")
 
-def cmdsend():
-    sendedbox = Frame(fenetre, width=100, height=10)
-    sendedbox.pack(side="top")
-    
-    sended = Label(sendedbox, width=40)
-    sended.pack(side="top")
-    
-def cmdbox():
-    cmdbox = Frame(fenetre)
-    cmdbox.pack(side="bottom")
+    def cmdSend(self):
+        
+        """
+        Commande: envoie d'une commande mannuelle
+        """
+        
+        self.sended["text"] = self.varSend.get()
+        self.varSend.initialize("")
 
-    cmdSend = StringVar()
-    ligne_texte = Entry(cmdbox, textvariable=cmdSend, width=40)
-    ligne_texte.pack(side="left")
+    
+    def fwcmd(self):
+        
+        """
+        Commande: envoie d'une commande mannuelle
+                  avancer
+        """
+        self.sended["text"] = "Forward"
+        
+    def bwcmd(self):
+        
+        """
+        Commande: envoie d'une commande mannuelle
+                  reculer
+        """
+        self.sended["text"] = "Backward"
+               
+    def leftcmd(self):
+        
+        """
+        Commande: envoie d'une commande mannuelle
+                  aller a gauche
+        """
+        self.sended["text"] = "Left"
+    
+    def rightcmd(self):
+        
+        """
+        Commande: envoie d'une commande mannuelle
+                  aller a droite
+        """
+        self.sended["text"] = "Right"
+    
+    def Rleftcmd(self):
+        
+        """
+        Commande: envoie d'une commande mannuelle
+                  aller a gauche
+        """
+        self.sended["text"] = "Rotation Left"
+    
+    def Rrightcmd(self):
+        
+        """
+        Commande: envoie d'une commande mannuelle
+                  aller a droite
+        """
+        self.sended["text"] = "Rotation Right"
+                    
+            
+    def init_controler(self):
+        """
+        Panneau de controle manuelle Translation
+        """
+        self.fenetrebox = Frame(self.fenetre)
+        self.fenetrebox.pack(side="top", pady=10)
+        
+        self.boutton_fw = Button(self.fenetrebox, command=self.fwcmd, text="fw", width=10)
+        self.boutton_fw.pack(side="top")
+        
+        self.boutton_bw = Button(self.fenetrebox, command=self.bwcmd, text="bw", width=10)
+        self.boutton_bw.pack(side="bottom")
+        
+        self.boutton_left = Button(self.fenetrebox, command=self.leftcmd, text="left", width=10)
+        self.boutton_left.pack(side="left")
+    
+        self.boutton_right = Button(self.fenetrebox, command=self.rightcmd, text="right", width=10)
+        self.boutton_right.pack(side="right")    
+        
+        """
+        Panneau de controle manuelle Rotation
+        """
+        self.fenetreboxR = Frame(self.fenetre)
+        self.fenetreboxR.pack(side="top", pady=10)
+        
+        self.boutton_leftR = Button(self.fenetreboxR, command=self.Rleftcmd, text="Rot. left", width=10)
+        self.boutton_leftR.pack(side="left")
+    
+        self.boutton_rightR = Button(self.fenetreboxR, command=self.Rrightcmd, text="Rot. right", width=10)
+        self.boutton_rightR.pack(side="right")
+        
+    
+    def init_sender(self):
+        """
+        envoyer des commandes en manuelle
+        """
+        
+        self.cmdbox = Frame(self.fenetre)
+        self.cmdbox.pack(side="bottom")
+    
+        self.varSend = StringVar()
+        self.ligne_texte = Entry(self.cmdbox, textvariable=self.varSend, width=40)
+        self.ligne_texte.pack(side="left")
+    
+        self.boutton_send = Button(self.cmdbox, command=self.cmdSend, text="send", width=10)
+        self.boutton_send.pack(side="right")
 
-    boutton_send = Button(cmdbox, text="send", width=10)
-    boutton_send.pack(side="right")
 
-def fenetreeur():
-    
-    fenetrebox = Frame(fenetre)
-    fenetrebox.pack(side="top", pady=10)
-    
-    boutton_fw = Button(fenetrebox, text="fw", width=10)
-    boutton_fw.pack(side="top")
-    
-    boutton_bw = Button(fenetrebox, text="bw", width=10)
-    boutton_bw.pack(side="bottom")
-    
-    boutton_left = Button(fenetrebox, text="left", width=10)
-    boutton_left.pack(side="left")
+    def init_loger(self):
+        """
+        log des commandes
+        """
+        
+        self.fenetre = Tk()
+        self.fenetre.title("Commande")
 
-    boutton_right = Button(fenetrebox, text="right", width=10)
-    boutton_right.pack(side="right")    
+        self.sendedbox = Frame(self.fenetre, width=100, height=10)
+        self.sendedbox.pack(side="top")
+        
+        self.sended = Label(self.sendedbox, width=40)
+        self.sended.pack(side="top")    
     
     
-    fenetreboxR = Frame(fenetre)
-    fenetreboxR.pack(side="top", pady=10)
-    
-    boutton_leftR = Button(fenetreboxR, text="Rot. left", width=10)
-    boutton_leftR.pack(side="left")
-
-    boutton_rightR = Button(fenetreboxR, text="Rot. right", width=10)
-    boutton_rightR.pack(side="right")    
-    
-cmdsend()
-cmdbox()
-fenetreeur()
-fenetre.mainloop()
-
-"""
 if __name__ == '__main__':
-    cmdsend()
-    cmdbox()
-    fenetreeur()
-    fenetre.mainloop()
-"""
+    test = User_Interface()
